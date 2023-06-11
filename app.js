dfbb703b2785acfd187252d36c984784c5227cdf
Marc Pozzo <pozzomarc@gmail.com>
2023-06-11 17:07:59 -0300

    dm the user that requested the channel summary, and send the summary to them

--- app.js
+++ app.js
@@ -7,39 +7,41 @@ const token = process.env.SLACK_BOT_TOKEN;
 const openai = new OpenAI(process.env.OPENAI_API_KEY);
 
 exports.handler = async (event, context) => {
-  const body = querystring.parse(
-    Buffer.from(event.body, "base64").toString("utf-8")
-  );
-
-  const channelName = body.text;
-  let response = {};
-
   try {
-    const channelId = await getChannelId(channelName, token);
+    const body = querystring.parse(
+      Buffer.from(event.body, "base64").toString("utf-8")
+    );
+
+    const channelName = body.text;
+    const userId = body.user_id;
+    let response = {};
+
+    const channelId = await getChannelId(channelName);
     if (channelId) {
       response.message = `The ID of '${channelName}' is: ${channelId}`;
-      const messages = await getChannelMessages(channelId, token);
-      const filteredMessages = messages.filter((message) => !message.subtype);
-      const textCompilation = filteredMessages
-        .map((message) => message.text)
-        .join(" ");
 
-      response.summary = await summarizeSlackChannel(
-        channelName,
-        textCompilation
-      );
+      // Enqueue the summarization task in the background
+      await summarizeSlackChannelInBackground(channelName, channelId, userId);
     } else {
       response.message = `Channel '${channelName}' not found.`;
     }
-  } catch (error) {
-    response.message = "Error finding channel";
-    console.log(`Error occurred: ${error}`);
-  }
 
-  return response.summary;
+    // Return an immediate response to Slack
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response),
+    };
+  } catch (error) {
+    // Handle errors and include error message in the response
+    console.error(`Error occurred: ${error}`);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
 };
 
-async function getChannelId(channelName, token) {
+async function getChannelId(channelName) {
   const url = "https://slack.com/api/conversations.list";
 
   try {
@@ -68,7 +70,29 @@ async function getChannelId(channelName, token) {
   }
 }
 
-async function getChannelMessages(channelId, token) {
+async function summarizeSlackChannelInBackground(
+  channelName,
+  channelId,
+  userId
+) {
+  try {
+    const messages = await getChannelMessages(channelId);
+    // Here I filter out all the messages that contain subtype prop (such as welcoming messages)
+    const filteredMessages = messages.filter((message) => !message.subtype);
+    const textCompilation = filteredMessages
+      .map((message) => message.text)
+      .join(" ");
+    const summary = await summarizeSlackChannel(channelName, textCompilation);
+
+    // Send DM to the user that requested the channel summary
+    await sendSummaryToUser(userId, summary);
+  } catch (error) {
+    console.error("Error occurred while summarizing Slack channel:", error);
+    // Handle the error appropriately, e.g., log, notify, etc.
+  }
+}
+
+async function getChannelMessages(channelId) {
   const url = `https://slack.com/api/conversations.history?channel=${channelId}`;
 
   try {
@@ -94,7 +118,6 @@ async function getChannelMessages(channelId, token) {
   }
 }
 
-// Function to summarize Slack channel chat history
 async function summarizeSlackChannel(channelName, chatHistory) {
   // Set up the prompt
   const prompt = `Summarize the Slack channel: ${channelName}\n\n${chatHistory}`;
@@ -115,6 +138,42 @@ async function summarizeSlackChannel(channelName, chatHistory) {
     return summary;
   } catch (error) {
     console.error("Error:", error);
-    return null;
+    throw new Error("Failed to summarize Slack channel.");
+  }
+}
+
+async function sendSummaryToUser(userId, summary) {
+  try {
+    // Open a DM channel with the user
+    const response = await axios.post(
+      "https://slack.com/api/conversations.open",
+      {
+        users: userId,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    const dmChannelId = response.data.channel.id;
+
+    // Send a DM to the user
+    const message = {
+      channel: dmChannelId,
+      text: summary,
+    };
+
+    await axios.post("https://slack.com/api/chat.postMessage", message, {
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  } catch (error) {
+    console.error("Error occurred while sending summary to user:", error);
+    // Handle the error appropriately, e.g., log, notify, etc.
   }
 }
